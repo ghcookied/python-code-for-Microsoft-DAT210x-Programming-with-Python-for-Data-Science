e29659764ebe76ea43af9cbd32b46ee710dd52cd
David <ghcookied@users.noreply.github.com>
2016-12-16 22:40:30 +0000

    Parallel coordinates of wheat data

---
+++ visualisation/parallel_components_example.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import os
+
+from pandas.tools.plotting import parallel_coordinates
+
+# Look pretty...
+matplotlib.style.use('ggplot')
+
+
+#
+# TODO: Load up the Seeds Dataset into a Dataframe
+# It's located at 'Datasets/wheat.data'
+# 
+# .. your code here ..
+
+os.getcwd()
+os.chdir('C:/Users/David/Documents/Technology/Python/edX/DAT210x-master/Module3')
+
+wheat_df  = pd.read_csv('Datasets/wheat.data')
+
+wheat_df.describe()
+
+#
+# TODO: Drop the 'id', 'area', and 'perimeter' feature
+# 
+# .. your code here ..
+
+wheat_df = wheat_df.drop(labels = ['id','area','perimeter'], axis = 1)
+wheat_df.head()
+
+#
+# TODO: Plot a parallel coordinates chart grouped by
+# the 'wheat_type' feature. Be sure to set the optional
+# display parameter alpha to 0.4
+# 
+# .. your code here ..
+
+plt.figure()
+parallel_coordinates(wheat_df, 'wheat_type')
+plt.show()
+
+
